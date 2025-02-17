971102b990fe621592a5663dd6a2f74a16a417e3
espeonten <sydenteo@gmail.com>
2025-02-17 13:54:56 -0800

    Update sketch.js
    added platforms and did movement

--- sketch.js
+++ sketch.js
@@ -1,20 +1,32 @@
+//fix direction facing
+//put patterns on background and platforms using loop
+//add bullets
+//add more platforms
+
 var p1, p1I
 var p2, p2I
+var edges
+var platform1, platform2, platform3, platform4, platform5, pGroup
 
 function preload() {
-  p1I.loadImage("p1.png")
-  p2I.loadImage("p2.png")
+  p1I = loadImage("p1.png")
+  p2I = loadImage("p2.png")
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("white");
-
-  p1 = createSprite(width/2, height/2)
-  p1.addImage(p1I)
 
   edges = createEdgeSprites()
-  
+
+  p1 = createSprite(width/4, height)
+  p1.addImage(p1I)
+  p1.scale = 0.3
+
+  p2 = createSprite(width - 300, height)
+  p2.addImage(p2I)
+  p2.scale = 0.3
+
+  createPlatforms()
 }
 
 //runs continuously
@@ -22,5 +34,33 @@ function draw() {
   console.log(mouseX, mouseY)
   background("white")
 
+  
+  p1.velocityY += 0.2
+  p2.velocityY += 0.2
+
+  if(keyDown("a")) {
+    p1.x-= 6
+  }
+  if(keyDown("d")) {
+    p1.x+= 6
+  }
+  if(keyDown("w") && (p1.collide(edges[3]) || p1.collide(pGroup))) {
+    p1.velocityY= -13
+  }
+  
+
+  if(keyDown("left")) {
+    p2.x-= 6
+  }
+  if(keyDown("right")) {
+    p2.x+= 6
+  }
+  if(keyDown("up") && (p2.collide(edges[3]) || p2.collide(pGroup))) {
+    p2.velocityY= -13
+  }
+
+  collidePlatforms()
+  p1.collide(edges)
+  p2.collide(edges)
   drawSprites()
 }
