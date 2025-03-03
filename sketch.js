bdd155ec8059589c07d62ed2d5a9f47b8a6545f7
espeonten <sydenteo@gmail.com>
2025-03-03 13:59:03 -0800

    Update sketch.js
    fixed bullets and added health bars

--- sketch.js
+++ sketch.js
@@ -9,6 +9,13 @@ var edges
 var platform1, platform2, platform3, platform4, platform5, pGroup
 var p1facing = "right"
 var p2facing = "left"
+var p1healthGreenHealthBar, p1healthRedHealthBar 
+var p1healthRedHealthBarWidth = 1
+var p2healthGreenHealthBar, p2healthRedHealthBar
+var p2healthRedHealthBarWidth = 1
+var bGroup
+var p1Score = 1
+var p2Score = 1
 
 function preload() {
   p1I = loadAnimation("p1.png")
@@ -32,6 +39,21 @@ function setup() {
   p2.addAnimation("p2 right", p22I)
   p2.scale = 0.3
 
+  p1healthGreenHealthBar = createSprite(p1.x, p1.y - 150, 120, 20)
+  p1healthGreenHealthBar.shapeColor = "green"
+
+  p1healthRedHealthBar = createSprite(p1.x, p1.y - 150, p1healthRedHealthBarWidth, 20)
+  p1healthRedHealthBar.shapeColor = "red"
+
+
+  p2healthGreenHealthBar = createSprite(p2.x -30, p2.y - 150, 120, 20)
+  p2healthGreenHealthBar.shapeColor = "green"
+
+  p2healthRedHealthBar = createSprite(p2.x -30, p2.y - 150, p2healthRedHealthBarWidth, 20)
+  p2healthRedHealthBar.shapeColor = "red"
+
+  bGroup = new Group()
+
   createPlatforms()
 }
 
@@ -40,6 +62,18 @@ function draw() {
   console.log(mouseX, mouseY)
   background("white")
 
+  p1healthGreenHealthBar.x = p1.x
+  p1healthGreenHealthBar.y = p1.y - 100
+  p1healthRedHealthBar.x = p1.x
+  p1healthRedHealthBar.y = p1.y - 100
+
+  p2healthGreenHealthBar.x = p2.x
+  p2healthGreenHealthBar.y = p2.y - 100
+  p2healthRedHealthBar.x = p2.x
+  p2healthRedHealthBar.y = p2.y - 100
+
+  p1healthRedHealthBar.width = p1Score
+  p2healthRedHealthBar.width = p2Score
 
   if(keyDown("a")) {
     p1.x-= 6
@@ -56,7 +90,6 @@ function draw() {
   }
   
 
-  
   if(keyDown("up") && (p2.collide(edges[3]) || p2.collide(pGroup))) {
     p2.velocityY= -13
   }
@@ -78,42 +111,65 @@ function draw() {
     ableToShoot=false
   }
 
+  if(p1Score == 120) {
+    pGroup.destroyEach()
+    p1.destroy()
+    p1healthRedHealthBar.destroy()
+    p1healthGreenHealthBar.destroy()
+  }
+  if(p2Score == 120) {
+    pGroup.destroyEach()
+    p2.destroy()
+    p2healthRedHealthBar.destroy()
+    p2healthGreenHealthBar.destroy()
+  }
+  
   collidePlatforms()
   p1.collide(edges)
   p2.collide(edges)
   p1.velocityY += 0.2
   p2.velocityY += 0.2
   shootBullet()
+  if(p1.isTouching(bGroup)) {
+    p1Score += 0.5
+  }
+  if(p2.isTouching(bGroup)) {
+    p2Score += 0.5
+  }
   drawSprites()
 }
 
 function shootBullet() {
-  if(keyDown("s") && ableToShoot == true) {
+  if(keyDown("s") && ableToShoot == true && p1Score < 120 && p2Score < 120) {
     if(p1facing == "right") {
       bullet = createSprite(width/2,height/2, 20, 10)
-      bullet.x = p1.x + 60
+      bullet.x = p1.x + 100
       bullet.y = p1.y
       bullet.velocityX = 30
+      bGroup.add(bullet)
     }
     else if(p1facing == "left") {
       bullet = createSprite(width/2,height/2, 20, 10)
-      bullet.x = p1.x - 60
+      bullet.x = p1.x - 100
       bullet.y = p1.y
       bullet.velocityX = -30
+      bGroup.add(bullet)
     }
   }
-  if(keyDown("down") && ableToShoot == true) {
+  if(keyDown("down") && ableToShoot == true && p2Score < 120 && p1Score < 120) {
     if(p2facing == "right") {
       bullet = createSprite(width/2,height/2, 20, 10)
-      bullet.x = p2.x + 60
+      bullet.x = p2.x + 100
       bullet.y = p2.y
       bullet.velocityX = 30
+      bGroup.add(bullet)
     }
     else if(p2facing == "left") {
       bullet = createSprite(width/2,height/2, 20, 10)
-      bullet.x = p2.x - 60
+      bullet.x = p2.x - 100
       bullet.y = p2.y
       bullet.velocityX = -30
+      bGroup.add(bullet)
     }
   }
 }
